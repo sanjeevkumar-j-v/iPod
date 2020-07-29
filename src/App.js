f74d761aecb098286a8db2147d3c8312a9f05f38
sanjeev kumar <sanjeevkumarjv@gmail.com>
2020-07-29 23:40:34 +0530

    Add the html and css files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
+
 function App() {
   return (
     <div >
-      <h1> Ipod</h1>
+      
+    <div className="main">
+        <div className="screen">
+            <div className="left">
+                <span  > Ipod</span>
+                <li className="active">Songs</li>
+                <li>Albums</li>
+                <li>Artists</li>
+                <li>Playlists</li>
+            </div>
+            <div className="right">
+                <div>
+                    Hi
+                </div>
+            </div>
+        </div>
+        <div className="navigator">
+            <div id="keys">
+                
+                <div className="menu-btn">
+                    MENU
+                </div>
+                <div className="fwd">
+                    <i className="fas fa-forward"></i>
+                </div>
+                <div className="bkd">
+                    <i className="fas fa-backward"></i>
+                </div>
+                <div className="play-pause">
+                    <i className="fas fa-play"></i>
+                    
+                </div>
+            </div>
+            <div className="play">
+            </div>
+            
+        </div>
+    </div>
+
     </div>
   );
 }
